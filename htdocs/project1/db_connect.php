1382618d3fda858327f9247195a4b0417a518f5f
Joshua Stemmler <stemmler010@yahoo.com>
2013-02-23 19:16:36 -0500

    Added the basic shell for the website, included for the first sprint is a search field (i'll do that) login (obviously accesses the database, but only in one statement so only one person can write it) and a registration page (can be done in to statements (one selects usernames to check if username is taken and second is used insert the new username and password into a login table). Now, this is only four instances of database access and we need 5. What we can do in the meantime, is have someone populate a gift table with a list of gift ideas and have a few of them show up on the index page, for now as a list and we will make it look pretty later

---
+++ htdocs/project1/db_connect.php
@@ -0,0 +1,5 @@
+<?php
+
+  	$db = mysqli_connect('localhost', 'root', 'stemmler', 'project1')
+		or die (mysqli_error());
+?>
---
+++ htdocs/project1/results.php
@@ -0,0 +1,40 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cz"><head><title>Company name - title</title>
+
+<meta http-equiv="Content-language" content="cs">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content=" ">
+<meta name="keywords" content=" ">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<meta name="author" content="Klára Frolichová, Sunlight webdesign">
+<meta name="Copyright" content="Klára Frolichová, Sunlight webdesign 2007">
+<meta name="design" content="Sunlight webdesign - http://www.sunlight.cz, info@sunlight.cz">
+<link rel="stylesheet" type="text/css" href="default.css" title="default">
+</head>
+
+
+<body>
+<div id="WholePage">
+<div id="Inner">
+<div id="Container">
+<div id="Head">
+<div id="Head_left">
+<div id="Leaf_top"></div>
+<div id="Leaf_bottom"> <a class="registration" href="?page=home">REGISTRATION</a> <a class="log-in" href="?page=home">LOG IN</a> </div>
+</div>
+<div id="Head_right">
+<div id="Logo">
+<div id="Name"><span class="blue"></span><span>Find A Gift</span></div>
+<div id="Informations">Home of all your gift idea needs </div>
+</div>
+<div id="Top_menu"> <a class="kart" href="?page=home"><span>CATAGORIES</span></a> 
+<a class="contact" href="?page=home"><span>ABOUT</span></a>
+<a class="contact" href="?page=home"><span>CONTACT</span></a>
+<a class="help" href="?page=home"><span>HELP</span></a>
+<a class="home" href="index.html"><span>HOME</span></a>
+</div>
+
+</div>
+</div>
+
+</body></html>
