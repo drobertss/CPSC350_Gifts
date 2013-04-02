7396c270b23758a234753c32e5cd573a3ae542fe
acropp <acropp@mail.umw.edu>
2013-04-02 00:35:29 -0400

    adding About Us page

---
+++ htdocs/project1/aboutus.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cz"><head><title>About Us</title>
+
+<meta http-equiv="Content-language" content="cs">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content=" ">
+<meta name="keywords" content=" ">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<meta name="author" content="Klára Frolichová, Sunlight webdesign">
+<meta name="Copyright" content="Klára Frolichová, Sunlight webdesign 2007">
+<meta name="design" content="Sunlight webdesign - http://www.sunlight.cz, info@sunlight.cz">
+<link rel="stylesheet" type="text/css" href="default.css" title="default">
+</head>
+
+<p> We here at Find A Gift think that buying gifts for your loved ones should be effortless! We strive to help you, our dedicated clients, 
+think less about those pesky birthdays and anniversaries! Imagine a world in which you have hours and hours of free time. Do you know what 
+that free time would be consumed with in the real world? Thinking of gift ideas! You have so many other things to worry about in your life,
+don't you? So let us worry about this one. Here at Find A Gift, we've got your back! 
+</p>
+
+
+<div id="Bottom">
+<p class="down">Copyright © 2007, <a href="http://www.sunlight.cz">Design by: Sunlight webdesign</a></p>
+<p class="down"><a href="http://www.sunlight.cz">Supported by </a><a href="http://www.hosting24.com/" target="_blank">Hosting24.com</a>
+</p>
+<div class="down2">
+<p> <a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88"></a>
+</p>
+</div>
+</div>
