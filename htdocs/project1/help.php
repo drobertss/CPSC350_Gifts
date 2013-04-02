a06b713069fdafe73b58d62a188279207aa5a3bb
rpatters2468 <rpatters@mail.umw.edu>
2013-04-02 11:05:12 -0400

    Commit from Robert Patterson.

---
+++ htdocs/project1/help.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Help Tab</title>
+ </head>
+ <body>
+ <?php echo '<p>Getting Started:
+ How To Register: To Register, you select the option to create an account on the home screen.
+ How To Login: To login, you select the login option on the main page as well.
+ Searching For A Gift: Once you are logged in, you can search gifts by the name in the search bar at the top.
+ Searching For Gifts By Category:  You can also browse the kind of gift you want by the category.
+ Saving Gift Ideas: Once you found a gift, you can save it your profile, which can then be viewed in your profile by clicking your username in the top left.
+ 
+ 
+ </p>'; ?> 
+ </body>
+</html>
