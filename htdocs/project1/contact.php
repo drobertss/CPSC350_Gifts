c2e67d98e8d509dbec168d8e508e84bc3765cc05
drobertss <robjarvis92@gmail.com>
2013-04-02 03:10:55 -0400

    contact page for additional questions

---
+++ htdocs/project1/contact.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cz"><head><title>About Us</title>
+
+<meta http-equiv="Content-language" content="cs">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content=" ">
+<meta name="keywords" content=" ">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<meta name="author" content="Klára Frolichová, Sunlight webdesign">
+<meta name="Copyright" content="Klára Frolichová, Sunlight webdesign 2007">
+<meta name="design" content="Sunlight webdesign - http://www.sunlight.cz, info@sunlight.cz">
+<link rel="stylesheet" type="text/css" href="default.css" title="default">
+</head>
+
+<p> 
+Contacting us is super easy!  You can e-mail us at: help@findgifts.com
+</p>
+<p>
+      
+</p>
+
+<p>
+Or just call us at 540-867-5309
+</p>
+<p>
+          
+</p>
+<p>
+Our international world headquarters are located in beautiful Fredericksburg, Virginia! In Trinkle B12 at the University of Mary Washington Campus.
+
+
+</p>
+
+
+
+<div id="Bottom">
+<p class="down">Copyright © 2007, <a href="http://www.sunlight.cz">Design by: Sunlight webdesign</a></p>
+<p class="down"><a href="http://www.sunlight.cz">Supported by </a><a href="http://www.hosting24.com/" target="_blank">Hosting24.com</a>
+</p>
+<div class="down2">
+<p> <a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88"></a>
+</p>
+</div>
+</div>
